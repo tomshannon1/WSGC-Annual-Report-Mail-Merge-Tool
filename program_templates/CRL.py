c030a9724aa593675c6df1e46f08051ef14f9321
Tom Shannon <tshannon@carthage.edu>
2017-12-17 13:40:02 -0600

    Updated CEL template

--- program_templates/CRL.py
+++ program_templates/CRL.py
@@ -2,6 +2,17 @@ from __future__ import print_function
 from mailmerge import MailMerge
 from datetime import date
 
+class CRLTemplate():
+
+    def __init__(self, document):
+
+        self.document = document
+    
+        self.__parse_document()
+
+    def __parse_document(self):
+
+
 template = "WSGC-Annual Report 2017.docx"
 
 CRLDictionary = CRLSheet
@@ -13,32 +24,34 @@ document.merge(
     Advisor=CRLDictionary["Advisor"],
     Project=CRLDictionary["Project"],
     
-    TeamMember1=CRLDictionary["Team Member 1"],
-    Status1=CRLDictionary["Status 1"],
-    Award1=CRLDictionary["Student Award"],
+    TeamMember1=CRLDictionary["TeamMember 1"],
+    Status1=CRLDictionary["Status1"],
+    Award1=CRLDictionary["StudentAward"],
     
-    TeamMember2=CRLDictionary["Team Member 2"],
-    Status2=CRLDictionary["Status 2"],
-    Award2=CRLDictionary["Student Award"],
+    TeamMember2=CRLDictionary["TeamMember 2"],
+    Status2=CRLDictionary["Status2"],
+    Award2=CRLDictionary["StudentAward"],
     
-    TeamMember3=CRLDictionary["Team Member 3"],
-    Status3=CRLDictionary["Status 3"],
-    Award3=CRLDictionary["Student Award"]
+    TeamMember3=CRLDictionary["TeamMember 3"],
+    Status3=CRLDictionary["Status3"],
+    Award3=CRLDictionary["StudentAward"]
     
-    TeamMember4=CRLDictionary["Team Member 4"],
-    Status4=CRLDictionary["Status 4"],
-    Award4=CRLDictionary["Student Award"],
+    TeamMember4=CRLDictionary["TeamMember4"],
+    Status4=CRLDictionary["Status4"],
+    Award4=CRLDictionary["StudentAward"],
     
-    TeamMember5=CRLDictionary["Team Member 5"],
-    Status5=CRLDictionary["Status 5"],
-    Award5=CRLDictionary["Student Award"]
+    TeamMember5=CRLDictionary["TeamMember5"],
+    Status5=CRLDictionary["Status5"],
+    Award5=CRLDictionary["StudentAward"]
     
-    TeamMember6=CRLDictionary["Team Member 6"],
-    Status6=CRLDictionary["Status 6"],
-    Award6=CRLDictionary["Student Award"]
+    TeamMember6=CRLDictionary["TeamMember6"],
+    Status6=CRLDictionary["Status6"],
+    Award6=CRLDictionary["StudentAward"]
     
     
-    Absract=CRLDictionary["Abstract"])
+    Absract=CRLDictionary["Abstract"],
+    CongressionalDistrict=CRL
+    )
     
 document.write('test-output.docx')
 
