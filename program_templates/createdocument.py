6ab319b44711fb081c50ca4822dbb7cc6fb73de9
Tom Shannon <tshannon@carthage.edu>
2017-12-18 12:16:32 -0600

    Ran program and received annual report output
    Need to add functionality to merge by district. Also add UGS, OPP, and USIP

--- program_templates/createdocument.py
+++ program_templates/createdocument.py
@@ -4,17 +4,19 @@ from datetime import date
 
 class WriteDocuments():
 
-    def __init__(self, template, fields):
+    def __init__(self, template, fields, sheet_name):
         
         self.template = template
         
         self.fields = fields
+        
+        self.sheet_name = sheet_name
     
         self.__parse_document()
 
     def __parse_document(self):
 
-        for entry, recipient in enumerate(self.fields, 1):
+        for entry, recipient in enumerate(self.fields, start=0):
             
             # Create a document based off denoted template
             document = MailMerge(self.template)
@@ -29,5 +31,5 @@ class WriteDocuments():
             document.merge_pages([recipient])
             
             # Write the document to the corresponding folder based on Congressional District
-            document.write('/Users/tomshannon/Documents/GitHub/WSGC-Annual-Report-Mail-Merge-Tool/program_templates/CD%d/CRL_%d.docx' % (district, entry))
+            document.write('/Users/tomshannon/Documents/GitHub/WSGC-Annual-Report-Mail-Merge-Tool/program_templates/CD%d/%s_%d.docx' % (district, self.sheet_name, entry))
 
